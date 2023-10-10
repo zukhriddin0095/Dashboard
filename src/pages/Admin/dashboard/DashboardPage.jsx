904822d12f5ab6c1c38e46607c48194613a14c2d
zukhriddin0095 <nurmatovzukhriddin4@gmail.com>
2023-10-10 11:08:33 +0500

    total add

--- src/pages/Admin/dashboard/DashboardPage.jsx
+++ src/pages/Admin/dashboard/DashboardPage.jsx
@@ -1,11 +1,21 @@
 import { Fragment } from "react";
+import { useGetPortfoliosQuery } from "../../../redux-tookit/services/PortfolioService";
+import { useGetUsersQuery } from "../../../redux-tookit/services/UsersService";
 
-
-import "./dashboard.scss"
+import "./dashboard.scss";
 const DashboardPage = () => {
+  const { data } = useGetPortfoliosQuery();
+
+  const User = useGetUsersQuery();
+  console.log(User.data);
+
   return (
     <Fragment>
-      
+      <div className="total">
+        <h3>Portfolios Total: ({data?.pagination?.total})</h3>
+        <h3>Skills Total: ({})</h3>
+        <h3>Users Total: ({User?.data?.pagination?.total})</h3>
+      </div>
     </Fragment>
   );
 };
